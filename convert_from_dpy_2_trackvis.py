b426bb46b6709e903cfb63f28c11eab9c2895e08
Nguyen Thien Bao <ntbaovn@gmail.com>
2013-03-11 16:03:53 +0100

    edit the iteration for run on the folder

--- convert_from_dpy_2_trackvis.py
+++ convert_from_dpy_2_trackvis.py
@@ -11,22 +11,27 @@ import numpy as np
 import nibabel as nib
 from dipy.io.dpy import Dpy
 #from dipy.io.pickles import load_pickle,save_pickle
+tracks_filename_arr=['tracks_dti_10K_linear.dpy','tracks_dti_1M_linear.dpy','tracks_dti_3M_linear.dpy',
+                     'tracks_dti_10K.dpy','tracks_dti_1M.dpy','tracks_dti_3M.dpy']
 
+trackvis_filename_arr=['tracks_dti_10K_linear.trk','tracks_dti_1M_linear.trk','tracks_dti_3M_linear.trk',
+                       'tracks_dti_10K.trk','tracks_dti_1M.trk','tracks_dti_3M.trk']
       
-dirname = "data_temp"
+#dirname = "ALS/ALS_temp"
+dirname = "ALS/ALS_Data/202"
 for root, dirs, files in os.walk(dirname):
-    if root.endswith('DTI'):
+  if root.endswith('DTI'):
     #if root.endswith('101_32'):
-        base_dir = root+'/'       
-        basedir_recon = os.getcwd() + '/' + base_dir #+ 'DTI/'               
+    base_dir = root+'/'       
+    basedir_recon = os.getcwd() + '/' + base_dir + '/'               
         
         #loading fa_image
-        fa_file = base_dir + 'fa.nii.gz'            
-        fa_img = nib.load(fa_file)
-        fa = fa_img.get_data()
-        fa[np.isnan(fa)] = 0
-                         
-        tracks_filename = basedir_recon + 'tracks_dti_10K.dpy'
+    fa_file = basedir_recon + 'fa.nii.gz'            
+    fa_img = nib.load(fa_file)
+    fa = fa_img.get_data()
+    fa[np.isnan(fa)] = 0
+    for i in range(len(tracks_filename_arr)):
+        tracks_filename = basedir_recon + tracks_filename_arr[i]
         dpr_tracks = Dpy(tracks_filename, 'r')
         tensor_tracks=dpr_tracks.read_tracks();
         dpr_tracks.close()
@@ -47,7 +52,7 @@ for root, dirs, files in os.walk(dirname):
 
         tensor_streamlines_trk = ((sl, None, None) for sl in tensor_tracks)
 
-        ten_sl_fname = basedir_recon + 'tracks_dti_10K.trk'
+        ten_sl_fname = basedir_recon + trackvis_filename_arr[i]
 
         """
         Save the streamlines.
@@ -55,7 +60,10 @@ for root, dirs, files in os.walk(dirname):
 
         nib.trackvis.write(ten_sl_fname, tensor_streamlines_trk, hdr, points_space='voxel')
         
-        print 'Done'
+        print '        Create', ten_sl_fname
+    print '>>>>>>>>>> Done ', base_dir 
+    
+        
 
 
 
