f8689234ce1a8d656c4cad463f73c5c476e8e245
Nguyen Thien Bao <ntbaovn@gmail.com>
2013-06-25 15:23:20 +0200

    minor change

--- 1_bet_and_Eddy.py
+++ 1_bet_and_Eddy.py
@@ -11,29 +11,32 @@ import os
 visualize = False
 #dirname = "data/1891215"
 #dirname = "ADHD"
-dirname = "Sample/2020162"
+#dirname = "Sample/2020162"
+dirname = "PBC2009/brain1_fr_raw"
 for root, dirs, files in os.walk(dirname):    
-    if root.endswith('MPRAGE_1'):
-        '''   anatomy bet    '''
+    
+    if root.endswith('MPRAGE_1_from_structual_T1Space'):
+        #   anatomy bet    
         base_dir = root+'/'
-        filename = 'defaced_MPRAGE.nii.gz'
+        #filename = 'T1_anatomy.nii.gz'
+        filename = 'fbrain1_mprage1.nii.gz'        
         base_filename = base_dir + filename        
 
-        bet(base_filename, root+'/raw_bet.nii.gz',options=' -R -f .2 -g 0')
+        #bet(base_filename, root+'/fbrain0_mprage1_bet.nii.gz',options=' -R -f .2 -g 0')
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -F -f .4 -g -0.05 -c 151 177 90')
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -F -f .2 -g -0.02 -c 151 177 90')
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -F -f .1 -g -0.02 -c 151 177 90')         
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -F -f .15 -g 0.0 -c 151 177 90')                        
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -R -f .2 -g 0.0 -c 151 177 90')                
         
-        #bet(base_filename, root+'/raw_bet.nii.gz',options=' -R -f .2 -g 0.0' )      #-c 151 177 90')                
+        bet(base_filename, root+'/bet_new.nii.gz',options=' -R -f .2 -g 0.0 -c 151 177 90')                
+    '''    
+    if root.endswith('DTI'):
         
-    if root.endswith('DTI64_1'):
-        '''
-        difusion tensor image - brain extraction 
-        '''
+        #difusion tensor image - brain extraction 
+        
         base_dir = root+'/'
-        filename = 'DTI64.nii.gz'
+        filename = 'raw.nii.gz'        
         base_filename = base_dir + filename        
 
         bet(base_filename, root+'/raw_bet.nii.gz',options=' -R -F -f .2 -g 0')
@@ -44,8 +47,8 @@ for root, dirs, files in os.walk(dirname):
         #bet(base_filename, root+'/rawbet.nii.gz',options=' -R -f .2 -g 0.0 -c 151 177 90')                
         
         #bet(base_filename, root+'/raw_bet.nii.gz',options=' -R -f .2 -g 0.0' )      #-c 151 177 90')                  
-        '''
-        eddy correction 
-        '''
+        
+        #eddy correction 
+        
         eddy_correct(root+'/raw_bet.nii.gz',root+'/raw_bet_ecc.nii.gz')#,ref=0)
-     
+     '''
