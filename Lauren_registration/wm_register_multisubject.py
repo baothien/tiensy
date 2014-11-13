958f19900664345dde472eedc500e992f9b120a9
Nguyen Thien Bao <ntbaovn@gmail.com>
2014-11-13 16:39:02 +0100

    minor changes

--- Lauren_registration/wm_register_multisubject.py
+++ Lauren_registration/wm_register_multisubject.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#!/usr/lib/python2.7
 # Run registration on the test dataset.
 
 import os
@@ -24,18 +24,19 @@ fiber_sample_fractions = [.10, .20, .30, .40]
 sigma_per_scale = [30, 10, 10, 5]
 # on a 27-subject dataset, 12 or 14 processors, this takes nearly 24 hours.
 # the implementation is not fast, but this is a long time to wait.
-#steps_per_scale=[10, 3, 2, 2]
+steps_per_scale=[10, 3, 2, 2]
 # the big gain in objective from the 1st scale is early,
 # and actually after the fine (3rd scale) registration
 # the result looks good enough. So, save output after third step,
 # and also reduce the time spent in first and last scales.
 # note: this is still more computation than in the publication.
 # Trying to decide how much computation is needed for desired result quality.
-steps_per_scale=[1, 1, 1, 1]
+
+#steps_per_scale=[1, 1, 1, 1]
 #steps_per_scale=[5, 3, 2, 1]
 
-fibers_rendered = 10
-#fibers_rendered = 100
+#fibers_rendered = 10
+fibers_rendered = 200
 
 # figure out how many cobyla iterations are needed
 # this is reasonable for two subjects
